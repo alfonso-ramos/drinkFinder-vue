4a92dfd99170ca7a34e84b7113fefa4dc6b08f92
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-12-18 12:20:07 -0700

    [ADD] Inicio de proyecto, maquetado el router y formulario

---
+++ src/views/FavoritosView.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+
+<template>
+  <h1 class="text-6xl font-extrabold">Favoritos</h1>
+</template>
---
+++ src/views/InicioView.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+
+<template>
+  <h1 class="text-6xl font-extrabold">Recetas</h1>
+</template>
